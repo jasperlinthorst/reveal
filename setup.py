5ea9f8c1ef32076cdcd05f04aed9b27bb0243656
jasperlinthorst <jasper.linthorst@gmail.com>
2015-05-05 17:32:33 +0200

    Fixed dependency naming error

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ ez_setup.use_setuptools()
 from setuptools import setup, Extension
 
 setup(name="reveal", version="0.1",
-	install_requires=['seqal==0.1',],
-	dependency_links = ['https://github.com/mhulsman/seqal/archive/seqal_python_v0.1.tar.gz#egg=seqal-0.1'],
+	install_requires=['seqalign==0.1',],
+	dependency_links = ['https://github.com/mhulsman/seqal/archive/seqal_python_v0.1.tar.gz#egg=seqalign-0.1'],
 	scripts = ['reveal'],
 	ext_modules=[ \
 		Extension("GSA_64", ["GSA.c"], libraries=['z','divsufsort64'], undef_macros=['NDEBUG'], define_macros = [('LARGEIDX', '1')], extra_compile_args=['-O3']), \
