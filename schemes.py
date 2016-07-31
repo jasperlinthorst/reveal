4b118030da56f76e330d30e02ca5d7ca6b0b05a3
Jasper Linthorst <jasper.linthorst@gmail.com>
2016-07-31 21:14:49 +0200

    Added some documentation in the form of ipython notebooks

--- schemes.py
+++ schemes.py
@@ -12,110 +12,14 @@ import sys
 global minlength, minscore, pcutoff
 minlength=20
 minscore=0
-pcutoff=1e-3
 ts=IntervalTree()
 G=nx.DiGraph()
-
-#take multi-mum that is observed in all samples until it drops below maxl
-#then, check calculate significance for the multimum if it's not significant
-#take the next multi-mum that is not observed in all samples but is significant
-def mumpicker(multimums,idx):
-    maxscore=0
-    maxl=0
-    nomum=None
-    #nomum=(0,idx,0,[])
-    bestmum=nomum
-    for multimum in multimums:
-        l,n,sp=multimum
-        if n==idx.nsamples:
-            if l>maxl:
-                maxl=l
-                bestmum=(l,idx,n,sp)
-    if bestmum==None:
-        return None
-    p=(1-((1-(0.25**bestmum[0]))**(((idx.n/float(idx.nsamples))**2))))**(idx.nsamples-1)
-    if p>pcutoff:
-        bestmum=(0,idx,0,[])
-        multimums.sort(key=lambda l: l[0]*l[1],reverse=True)
-        for multimum in multimums:
-            l,n,sp=multimum
-            p=(1-((1-(0.25**l))**(((idx.n/idx.nsamples)**2))))**(idx.nsamples-1)
-            if (p<=pcutoff):
-                return (l,idx,n,sp)
-        #not a single significant multi-mum
-        return nomum
-    else:
-        #one multimum for all samples index
-        return bestmum
-
-#take the largest multimum that occurs in all samples, until it drops below threshold
-#then take largest multimum in less samples that is above thresholds
-#TODO: calculate score for multi-mums!
-def mumpicker2(multimums,idx):
-    bestmum_by_n={}
-    bestmum=None    
-    for multimum in multimums:
-        l,n,sp=multimum
-        if n in bestmum_by_n:
-            if l>bestmum_by_n[n][0]:
-                bestmum_by_n[n]=(l,idx,n,0,sp)
-        else:
-            bestmum_by_n[n]=(l,idx,n,0,sp)
-    
-    for key in sorted(bestmum_by_n,reverse=True):
-        if bestmum_by_n[key][0]>=minlength:
-            bestmum=bestmum_by_n[key]
-            break
-        #else:
-        #    cluster(multimums,idx)
-    return bestmum
-
-#take the best scoring multimum that occurs in all samples, until it drops below threshold
-#then take largest multimum in less samples that is above thresholds
-#TODO: calculate score for multi-mums!
-def mumpicker3(multimums,idx):
-    bestmum_by_n={}
-    bestmum=None
-    for multimum in multimums:
-        l,n,sp=multimum
-        ds=[start-ts[start].pop()[0] for start in sp]
-        ads=sum(ds)/n
-        spenalty=sum([abs(p-ads) for p in ds])
-        de=[ts[start].pop()[1]-(start+l) for start in sp]
-        ade=sum(de)/n
-        epenalty=sum([abs(p-ade) for p in de])
-        penalty=min([spenalty,epenalty])
-        score=(l*(n**2))-penalty
-
-        if n in bestmum_by_n:
-            if score>bestmum_by_n[n][3]:
-                bestmum_by_n[n]=(l,idx,n,score,sp)
-                #print "better",l, n, score, penalty, spenalty, epenalty, sp, ds, de
-        else:
-            bestmum_by_n[n]=(l,idx,n,score,sp)
-            #print "initial",l, n, score, penalty, spenalty, epenalty, sp, ds, de
-    
-    for key in sorted(bestmum_by_n,reverse=True):
-        if bestmum_by_n[key][3]>=minscore:
-            bestmum=bestmum_by_n[key]
-            break
-    
-    #print "bestmum",bestmum
-    #print index.nsamples, bestmum_by_n, bestmum
-    return bestmum
 
 def multimumpicker(multimums,idx):
     try:
         bestmum=None
         trace=False
-
-        #if idx.left!=None and idx.right!=None:
-        #    leftoffsets=G.node[idx.left]['offsets']
-        #    rightoffsets=G.node[idx.right]['offsets']
-        #    if 'CTRI_2.fasta' in leftoffsets and 'CTRI_2.fasta' in rightoffsets:
-        #        if leftoffsets['CTRI_2.fasta']==341197 and rightoffsets['CTRI_2.fasta']==341395:
-        #            trace=True
-        
+ 
         for multimum in multimums:
             l,n,sp=multimum
             if l<minlength:
@@ -183,6 +87,14 @@ def graphmumpicker(mums,idx,penalize=True):
         if idx.right!=None:
             nrighttup=idx.right
             nright=G.node[Interval(nrighttup[0],nrighttup[1])]
+        
+
+        if idx.left!=None and idx.right!=None:
+            leftoffsets=G.node[idx.left]['offsets']
+            rightoffsets=G.node[idx.right]['offsets']
+            if '000068F' in leftoffsets and '000068F' in rightoffsets:
+                if leftoffsets['000068F']==201987 and rightoffsets['000068F']==202021:
+                    trace=True
         
         for mum in mums:
             #if trace:
@@ -300,8 +212,6 @@ def mindist(x,y):
         if mind==0:
             break
     return mind
-
-
 
 def printSA(index,maxline=100,start=0,end=200):
     sa=index.SA
