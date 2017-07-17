19b7abf4da0305c2c7b3da3edf239e4c3830a29e
Jasper Linthorst <jasper.linthorst@gmail.com>
2017-07-17 18:14:01 +0200

    Make reverse complements and uniqs conditional

--- reveal/matches.py
+++ reveal/matches.py
@@ -78,54 +78,74 @@ def matches(args):
     
     idx.construct()
     
-    print "##refname\trefstart\tctgname\tctgstart\tlength\tn\tunique\torient"
-    for mem in idx.getmems(args.minlength) :
-        refstart=mem[2][0]
-        ctgstart=mem[2][1]
-        rnode=t[refstart].pop() #start position on match to node in graph
-        cnode=t[ctgstart].pop()
-        print "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" % (rnode[2], refstart-rnode[0], cnode[2], ctgstart-cnode[0], mem[0], mem[1], mem[3], 0)
-    
-    logging.debug("Indexing reverse complement...\n")
-    
-    ### index reverse complement
-    if args.sa64:
-        idx=reveallib64.index(sa=args.sa2, lcp=args.lcp2) #enable preconstruction of second SA and LCP array
+    if args.uniq:
+        print "##refname\trefstart\tctgname\tctgstart\tlength\tn\torient"
+        for mem in idx.getmums(args.minlength):
+            refstart=mem[2][0]
+            ctgstart=mem[2][1]
+            rnode=t[refstart].pop() #start position on match to node in graph
+            cnode=t[ctgstart].pop()
+            print "%s\t%s\t%s\t%s\t%s\t%s\t%s" % (rnode[2], refstart-rnode[0], cnode[2], ctgstart-cnode[0], mem[0], mem[1], 0)
     else:
-        idx=reveallib.index(sa=args.sa2, lcp=args.lcp2) #enable preconstruction of second SA and LCP array
+        print "##refname\trefstart\tctgname\tctgstart\tlength\tn\tunique\torient"
+        for mem in idx.getmems(args.minlength):
+            refstart=mem[2][0]
+            ctgstart=mem[2][1]
+            rnode=t[refstart].pop() #start position on match to node in graph
+            cnode=t[ctgstart].pop()
+            print "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" % (rnode[2], refstart-rnode[0], cnode[2], ctgstart-cnode[0], mem[0], mem[1], mem[3], 0)
     
-    rcG=nx.DiGraph()
-    t=IntervalTree()
-    
-    idx.addsample(reffile)
-    if args.reference.endswith(".gfa"):
-        read_gfa(args.reference,idx,t,rcG)
-    else:
-        rcG.graph['samples']=set([reffile])
-        for name,seq in fasta_reader(args.reference):
-            intv=idx.addsequence(seq)
-            intv=Interval(intv[0],intv[1],name)
-            t.add(intv)
-            rcG.add_node(intv,offsets={reffile:0},aligned=0)
-            refseq=seq
-    
-    idx.addsample(ctgfile)
-    if args.contigs.endswith(".gfa"):
-        read_gfa(args.contigs,idx,t,rcG,revcomp=True)
-    else:
-        rcG.graph['samples']=set([ctgfile])
-        for name,seq in fasta_reader(args.contigs):
-            intv=idx.addsequence(rc(seq))
-            intv=Interval(intv[0],intv[1],name)
-            t.add(intv)
-            rcG.add_node(intv,offsets={ctgfile:0},aligned=0)
-    
-    idx.construct()
-    
-    for mem in idx.getmems(args.minlength):
-        refstart=mem[2][0]
-        ctgstart=mem[2][1]
-        rnode=t[refstart].pop() #start position on match to node in graph
-        cnode=t[ctgstart].pop()
-        l=cnode[1]-cnode[0]
-        print "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" % (rnode[2], refstart-rnode[0], cnode[2], l-((ctgstart-cnode[0])+mem[0]), mem[0], mem[1], mem[3], 1)
+    if args.rc:
+
+        logging.debug("Indexing reverse complement...\n")
+        
+        ### index reverse complement
+        if args.sa64:
+            idx=reveallib64.index(sa=args.sa2, lcp=args.lcp2) #enable preconstruction of second SA and LCP array
+        else:
+            idx=reveallib.index(sa=args.sa2, lcp=args.lcp2) #enable preconstruction of second SA and LCP array
+        
+        rcG=nx.DiGraph()
+        t=IntervalTree()
+        
+        idx.addsample(reffile)
+        if args.reference.endswith(".gfa"):
+            read_gfa(args.reference,idx,t,rcG)
+        else:
+            rcG.graph['samples']=set([reffile])
+            for name,seq in fasta_reader(args.reference):
+                intv=idx.addsequence(seq)
+                intv=Interval(intv[0],intv[1],name)
+                t.add(intv)
+                rcG.add_node(intv,offsets={reffile:0},aligned=0)
+                refseq=seq
+        
+        idx.addsample(ctgfile)
+        if args.contigs.endswith(".gfa"):
+            read_gfa(args.contigs,idx,t,rcG,revcomp=True)
+        else:
+            rcG.graph['samples']=set([ctgfile])
+            for name,seq in fasta_reader(args.contigs):
+                intv=idx.addsequence(rc(seq))
+                intv=Interval(intv[0],intv[1],name)
+                t.add(intv)
+                rcG.add_node(intv,offsets={ctgfile:0},aligned=0)
+        
+        idx.construct()
+        
+        if args.uniq:
+            for mem in idx.getmums(args.minlength):
+                refstart=mem[2][0]
+                ctgstart=mem[2][1]
+                rnode=t[refstart].pop() #start position on match to node in graph
+                cnode=t[ctgstart].pop()
+                l=cnode[1]-cnode[0]
+                print "%s\t%s\t%s\t%s\t%s\t%s\t%s" % (rnode[2], refstart-rnode[0], cnode[2], l-((ctgstart-cnode[0])+mem[0]), mem[0], mem[1], 1)
+        else:
+            for mem in idx.getmems(args.minlength):
+                refstart=mem[2][0]
+                ctgstart=mem[2][1]
+                rnode=t[refstart].pop() #start position on match to node in graph
+                cnode=t[ctgstart].pop()
+                l=cnode[1]-cnode[0]
+                print "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s" % (rnode[2], refstart-rnode[0], cnode[2], l-((ctgstart-cnode[0])+mem[0]), mem[0], mem[1], mem[3], 1)
