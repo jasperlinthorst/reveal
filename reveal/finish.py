12b8da5bf896089d78927bafc136af1af722483d
Jasper Linthorst <jasper.linthorst@gmail.com>
2017-11-15 18:17:56 +0100

    Properly extend rc matches as well

--- reveal/finish.py
+++ reveal/finish.py
@@ -247,7 +247,7 @@ def finish(args):
             if ctg==ctgs[-1]: #the last chain for this chromosome
                 lastrefchain=True
 
-            if ci==len(ctg2ref[ctgname])-1: #the last chain for this contig 
+            if (ci==len(ctg2ref[ctgname])-1 and not revcomp) or (ci==0 and revcomp): #the last chain for this contig 
                 lastctgchain=True
             else:
                 lastctgchain=False
@@ -287,13 +287,15 @@ def finish(args):
                 # if lastrefchain and lastctgchain: #extend chains into telomere, if no other chains on either contig or reference
                 if lastctgchain:
                     if revcomp:
-                        logging.debug("Extending chain, trying to discard: %s[%d:%d]"%(ctgname,0,ctgbegin))
-                        ref2ctg['unchained'].remove((ctgname,0,ctgbegin)) #no longer unplaced
-                        ctgbegin=0
+                        if ctgbegin!=0:
+                            logging.debug("Extending chain, trying to discard: %s[%d:%d]"%(ctgname,0,ctgbegin))
+                            ref2ctg['unchained'].remove((ctgname,0,ctgbegin)) #no longer unplaced
+                            ctgbegin=0
                     else:
-                        logging.debug("Extending chain, trying to discard: %s[%d:%d]"%(ctgname,ctgend,contig2length[ctgname]))
-                        ref2ctg['unchained'].remove((ctgname,ctgend,contig2length[ctgname])) #no longer unplaced
-                        ctgend=contig2length[ctgname]
+                        if ctgend!=contig2length[ctgname]:
+                            logging.debug("Extending chain, trying to discard: %s[%d:%d]"%(ctgname,ctgend,contig2length[ctgname]))
+                            ref2ctg['unchained'].remove((ctgname,ctgend,contig2length[ctgname])) #no longer unplaced
+                            ctgend=contig2length[ctgname]
 
                 if pctgname==ctgname and revcomp==prevcomp and ((pci==ci+1 and revcomp==1) or (pci==ci-1 and revcomp==0)) and i!=0: #are chains consecutive
                     logging.debug("Consecutive chains, no gap.")
@@ -348,26 +350,29 @@ def finish(args):
                     
                     if pctgname==None and ((ci==0 and revcomp==0) or (revcomp==1 and ci==len(ctg2ref[ctgname])-1)): #first chain of the chromosome and the contig
                         event='contig break'
-                        logging.debug("First chain of contig: %s."%ctgname)
                         if revcomp: #extend alignment if its the first chain of the chromosome (telomeres are tricky)
-                            ref2ctg['unchained'].remove((ctgname,ctgend,contig2length[ctgname])) #no longer unplaced
-                            gapsize=gapsize-(contig2length[ctgname]-ctgend)
-                            ctgend=contig2length[ctgname]
+                            if contig2length[ctgname]!=ctgend:
+                                ref2ctg['unchained'].remove((ctgname,ctgend,contig2length[ctgname])) #no longer unplaced
+                                gapsize=gapsize-(contig2length[ctgname]-ctgend)
+                                ctgend=contig2length[ctgname]
                         else:
-                            ref2ctg['unchained'].remove((ctgname,0,ctgbegin)) #no longer unplaced
-                            gapsize=gapsize-ctgbegin
-                            ctgbegin=0
+                            if ctgbegin!=0:
+                                ref2ctg['unchained'].remove((ctgname,0,ctgbegin)) #no longer unplaced
+                                gapsize=gapsize-ctgbegin
+                                ctgbegin=0
                             
                     elif ((ci==0 and revcomp==0) or (revcomp==1 and ci==len(ctg2ref[ctgname])-1)) and ((pci==len(ctg2ref[pctgname])-1 and prevcomp==0) or (prevcomp==1 and pci==0)): #consecutive contigs, no chains in between
                         event='contig break'
                         if revcomp: #extend alignment if its the first chain of the chromosome (telomeres are tricky)
-                            ref2ctg['unchained'].remove((ctgname,ctgend,contig2length[ctgname])) #no longer unplaced
-                            gapsize=gapsize-(contig2length[ctgname]-ctgend)
-                            ctgend=contig2length[ctgname]
+                            if contig2length[ctgname]!=ctgend:
+                                ref2ctg['unchained'].remove((ctgname,ctgend,contig2length[ctgname])) #no longer unplaced
+                                gapsize=gapsize-(contig2length[ctgname]-ctgend)
+                                ctgend=contig2length[ctgname]
                         else:
-                            ref2ctg['unchained'].remove((ctgname,0,ctgbegin)) #no longer unplaced
-                            gapsize=gapsize-ctgbegin
-                            ctgbegin=0
+                            if ctgbegin!=0:
+                                ref2ctg['unchained'].remove((ctgname,0,ctgbegin)) #no longer unplaced
+                                gapsize=gapsize-ctgbegin
+                                ctgbegin=0
 
                     else: #not first or last chain of contig, so has to be stuctural event
                         logging.debug("Non consecutive chains between %s [%d:%d:%d] and %s [%d:%d:%d]."%(pctgname,pctgbegin,pctgend,prevcomp,ctgname,ctgbegin,ctgend,revcomp))
@@ -630,7 +635,7 @@ def decompose_contig(ctg,mums,contiglength,maxgapsize=1500,minchainsum=1000,maxm
     
     nrefchroms=len(set([p[5] for p in paths]))
     
-    logging.debug("Found a total of %d chains that map to %d different reference chromosomes."%(len(paths),nrefchroms))
+    logging.debug("Found a total of %d chains for %s that map to %d different reference chromosomes."%(len(paths),ctg,nrefchroms))
      
     paths=sorted(paths,key=lambda c: c[0],reverse=True) #sort chains by alignment score
     
