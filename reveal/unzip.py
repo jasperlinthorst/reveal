29c839f2b7428efb20248c39422443e6eefcefa7
Jasper Linthorst <jasper.linthorst@gmail.com>
2018-03-15 18:01:21 +0100

    Clean up unzip

--- reveal/unzip.py
+++ reveal/unzip.py
@@ -1,8 +1,8 @@
-
 import utils
 import bubbles
 import networkx as nx
 import os
+import logging
 
 def unzip(args):
     if not args.graph[0].endswith(".gfa"):
@@ -27,63 +27,10 @@ def unzip(args):
 
 #determine uncertainty about bubble positions
 def unzip_graph(G,minunzip=0):
-    nid=G.number_of_nodes()
-    while nid in G:
-        nid+=1
-
-    # for node,data in G.nodes(data=True):
-    #     pred=list(G.predecessors(node))
-    #     suc=list(G.predecessors(node))
-
-    #     lw=0
-    #     l=len(G.node[node]['seq'])
-
-    #     if len(pred)>1 and len(suc)>1:
-    #         maxw=int(round((l-2 if l>2 else 0)/float(2)))
-    #         lw=rw=maxw if maxw<minunzip else minunzip
-    #     elif len(pred)>1 and len(suc)<=1:
-    #         lw=l-1
-    #     else:
-    #         continue
-
-    #     dele=[]
-    #     if lw>0:
-    #         negpaths=set()
-    #         pospaths=set()
-    #         ndata=data.copy()
-    #         ndata['seq']=G.node[node]['seq'][:lw]
-    #         data['seq']=data['seq'][lw:] #update
-    #         pn=nid
-    #         G.add_node(nid,**ndata)
-    #         nid+=1
-    #         for n0,n1,d in G.in_edges(node,data=True):
-    #             if d['oto']=='-':
-    #                 negpaths=negpaths.union(d['paths'])
-    #             else:
-    #                 pospaths=pospaths.union(d['paths'])
-    #                 G.add_edge(n0,pn,**d)
-    #                 dele.append((n0,n1))
-
-    #         # for n0,n1,d in G.out_edges(node,data=True):
-    #         #     if d['ofrom']=='-':
-    #         #         negpaths=negpaths.union(d['paths'])
-    #         #         # G.add_edge(pn,n1,**d)
-    #         #         dele.append((n0,n1))
-    #         #     else:
-    #         #         pospaths=pospaths.union(d['paths'])
-
-    #         if len(pospaths)>0:
-    #             G.add_edge(pn,node,ofrom='+',oto='+',paths=pospaths)
-            
-    #         # if len(negpaths)>0:
-    #             # G.add_edge(node,pn,ofrom='-',oto='-',paths=negpaths)
-    #     G.remove_edges_from(dele)
+    nid=max([n for n in G.nodes() if type(n)==int])
+    nid+=1
 
     for b in bubbles.bubbles(G):
-        # if b.issimple():
-        # if type(b.source)==str or type(b.sink)==str:
-        #     print "skipping"
-        #     continue
         nid=unzip_bubble(G,b,minunzip=minunzip,idoffset=nid)
 
 def unzip_bubble(G,b,minunzip=0,idoffset=0):
@@ -97,6 +44,7 @@ def unzip_bubble(G,b,minunzip=0,idoffset=0):
         wiggle=(0,wiggle[1])
 
     if wiggle!=(0,0):
+        logging.debug("Unzipping bubble between %s and %s"%(b.source,b.sink))
         srcl=len(G.node[b.source]['seq'])
         snkl=len(G.node[b.sink]['seq'])
         maxlw=int(round((srcl-2 if srcl>2 else 0)/float(2)))
@@ -152,7 +100,5 @@ def unzip_bubble(G,b,minunzip=0,idoffset=0):
                     idoffset+=1
                 else:
                     G.node[n]['seq']=G.node[n]['seq']+rs
-    else:
-        print "Skipping bubble unzip!"
 
     return idoffset
