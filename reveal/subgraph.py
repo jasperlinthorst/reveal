68bc5f1141edfbb1fc8d2d4639f3361eb2d22c43
Jasper Linthorst <jasper.linthorst@gmail.com>
2018-04-10 17:11:05 +0200

    Retrieve subgraphs by bed like interval definitions

--- reveal/subgraph.py
+++ reveal/subgraph.py
@@ -1,27 +1,35 @@
 import networkx as nx
 from utils import *
+from intervaltree import IntervalTree
 
 def subgraph(args):
     if len(args.inputfiles)<=1:
         logging.fatal("Specify 1 gfa file followed by comma separated list of node ids that make up the subgraph.")
         return
+
     if not args.inputfiles[0].endswith('.gfa'):
         logging.fatal("Specify gfa file as first argument of subgraph subcommand.")
         return
-    
+
     G=nx.DiGraph()
     read_gfa(args.inputfiles[0],None,"",G)
     
     nodes=set()
     
     for arg in args.inputfiles[1:]:
-        for node in arg.split(','):
-            if node.find('-')!=-1: #then bubble definition
-                source,sink=node.split('-')
-                for node in bubbles.bubble(G,source,sink).nodes:
-                    nodes.add(int(node))
-            else:
+        if arg.find(':'): #then parse as interval definition: eg chr4:230000-230010
+            cds,intv=arg.split(':')
+            tree=graph_to_tree(G,cds) #use interval tree for retrieval
+            start,stop=intv.split('-')
+            for tup in tree[int(start):int(stop)]:
+                print tup
+                nodes.add(tup[2])
+        elif arg.find('-')!=-1: #then bubble definition
+            source,sink=arg.split('-')
+            for node in bubbles.bubble(G,source,sink).nodes:
                 nodes.add(int(node))
+        else:
+            nodes.add(int(arg))
     
     sg=G.subgraph(nodes)
     if args.gml:
@@ -29,3 +37,11 @@ def subgraph(args):
     else:
         write_gfa(sg,"",outputfile=args.outfile, remap=False)
 
+def graph_to_tree(G,cds):
+    tree=IntervalTree()
+    cds=G.graph['path2id'][cds]
+    for node in G:
+        if type(node)!=str:
+            if cds in G.node[node]['offsets']:
+                tree[G.node[node]['offsets'][cds]:G.node[node]['offsets'][cds]+len(G.node[node]['seq'])]=node
+    return tree
