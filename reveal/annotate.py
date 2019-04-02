222ee7f463d4875764bab7b93c7483abc1eefa66
Jasper Linthorst <jasper.linthorst@gmail.com>
2019-04-02 14:33:45 +0200

    Use pysam

--- reveal/annotate.py
+++ reveal/annotate.py
@@ -5,27 +5,24 @@ import logging
 import uuid
 import gzip
 
-
 def annotate(args):
-    import vcf
-    from vcf.parser import _Info as VcfInfo
+    import pysam
 
-    vcf_reader = vcf.Reader(filename=args.vcffile)
+    vcf_reader=pysam.VariantFile(args.vcffile,"r")
 
     aid=0
-    # variant2aid={}
     aid2variant={}
     vi=0
 
     vfile=args.vcffile+".fasta"
     with open(vfile,'w') as v:
         for record in vcf_reader:
-            if 'diffsize' in record.INFO:
-                if record.INFO['diffsize']>=args.mindiff and record.INFO['diffsize']<args.maxdiff:
+            if 'diffsize' in record.info:
+                if record.info['diffsize']>=args.mindiff and record.info['diffsize']<args.maxdiff:
                     for i,allele in enumerate(record.alleles):
                         allele=str(allele)
                         aid+=1
-                        aid2variant[aid]=(record.CHROM,record.POS,i)
+                        aid2variant[aid]=(record.chrom,record.pos,i)
                         # variant2aid[(record.CHROM,record.POS,i)]=aid
                         # v.write(">%s\n"%str(record.CHROM+"_"+str(record.POS)+"_"+str(i)))
                         v.write(">%d\n"%aid)
@@ -36,8 +33,8 @@ def annotate(args):
     if vi==0:
         logging.info("No variants in this size range.")
         sys.exit(1)
-
-    vcf_reader = vcf.Reader(filename=args.vcffile)
+    
+    vcf_reader=pysam.VariantFile(args.vcffile,"r")
 
     repmd={}
     if args.repm:
@@ -59,11 +56,11 @@ def annotate(args):
         except OSError:
             pass
 
-        vcf_reader.infos['repm_rfamily']=VcfInfo('repm_rfamily', 1, 'String', 'Best match for RepeatMasker - Repeat family.',None,None)
-        vcf_reader.infos['repm_rtype']=VcfInfo('repm_rtype', 1, 'String', 'Best match for RepeatMasker - Repeat type.',None,None)
-        vcf_reader.infos['repm_rcov']=VcfInfo('repm_rcov', 1, 'Float', 'Best match for RepeatMasker - Fraction of the repeat annotation covered.',None,None)
-        vcf_reader.infos['repm_qcov']=VcfInfo('repm_qcov', 1, 'Float', 'Best match for RepeatMasker - Fraction of the indel covered.',None,None)
-        vcf_reader.infos['repm_allele']=VcfInfo('repm_allele', 1, 'Integer', 'The allele that contains the best RepeatMasker match.',None,None)
+        vcf_reader.header.info.add('repm_rfamily', 1, 'String', 'Best match for RepeatMasker - Repeat family.')
+        vcf_reader.header.info.add('repm_rtype', 1, 'String', 'Best match for RepeatMasker - Repeat type.')
+        vcf_reader.header.info.add('repm_rcov', 1, 'Float', 'Best match for RepeatMasker - Fraction of the repeat annotation covered.')
+        vcf_reader.header.info.add('repm_qcov', 1, 'Float', 'Best match for RepeatMasker - Fraction of the indel covered.')
+        vcf_reader.header.info.add('repm_allele', 1, 'Integer', 'The allele that contains the best RepeatMasker match.')
 
     trfd={}
     if args.trf:
@@ -82,76 +79,74 @@ def annotate(args):
         except OSError:
             pass
 
-        vcf_reader.infos['trf_copynumber']=VcfInfo('trf_copynumber', 1, 'Float', 'Best match for TRF - copynumber.',None,None)
-        vcf_reader.infos['trf_conssize']=VcfInfo('trf_conssize', 1, 'Integer', 'Best match for TRF - concensus size.',None,None)
-        vcf_reader.infos['trf_entropy']=VcfInfo('trf_entropy', 1, 'Float', 'Best match for TRF - entropy.',None,None)
-        vcf_reader.infos['trf_pattern']=VcfInfo('trf_pattern', 1, 'String', 'Best match for TRF - pattern.',None,None)
-        vcf_reader.infos['trf_start']=VcfInfo('trf_start', 1, 'String', 'Best match for TRF - start position of tr.',None,None)
-        vcf_reader.infos['trf_end']=VcfInfo('trf_end', 1, 'String', 'Best match for TRF - end position of tr.',None,None)
-        vcf_reader.infos['trf_gccontent']=VcfInfo('trf_gccontent', 1, 'Float', 'Best match for TRF - Fraction of GC bases in the repeat pattern.',None,None)
-        vcf_reader.infos['trf_percent_indel']=VcfInfo('trf_percent_indel', 1, 'Integer', 'Best match for TRF - percentage of indels within the aligned repeat pattern.',None,None)
-        vcf_reader.infos['trf_percent_match']=VcfInfo('trf_percent_match', 1, 'Integer', 'Best match for TRF - percentage of matches within the aligned repeat pattern.',None,None)
-        vcf_reader.infos['trf_allele']=VcfInfo('trf_allele', 1, 'Integer', 'The allele that contains the best TRF match.',None,None)
+        vcf_reader.header.info.add('trf_copynumber', 1, 'Float', 'Best match for TRF - copynumber.')
+        vcf_reader.header.info.add('trf_conssize', 1, 'Integer', 'Best match for TRF - concensus size.')
+        vcf_reader.header.info.add('trf_entropy', 1, 'Float', 'Best match for TRF - entropy.')
+        vcf_reader.header.info.add('trf_pattern', 1, 'String', 'Best match for TRF - pattern.')
+        vcf_reader.header.info.add('trf_start', 1, 'String', 'Best match for TRF - start position of tr.')
+        vcf_reader.header.info.add('trf_end', 1, 'String', 'Best match for TRF - end position of tr.')
+        vcf_reader.header.info.add('trf_gccontent', 1, 'Float', 'Best match for TRF - Fraction of GC bases in the repeat pattern.')
+        vcf_reader.header.info.add('trf_percent_indel', 1, 'Integer', 'Best match for TRF - percentage of indels within the aligned repeat pattern.')
+        vcf_reader.header.info.add('trf_percent_match', 1, 'Integer', 'Best match for TRF - percentage of matches within the aligned repeat pattern.')
+        vcf_reader.header.info.add('trf_allele', 1, 'Integer', 'The allele that contains the best TRF match.')
 
     #clean up
     os.remove(vfile)
     
-    vcf_reader.infos['reveal_type']=VcfInfo('reveal_type', 1, 'String', 'REVEAL\'s best guess at the type of variant.',None,None)
+    vcf_reader.header.info.add('reveal_type', 1, 'String', 'REVEAL\'s best guess at the type of variant.')
 
     if args.vcffile.endswith('.vcf.gz'):
         outputfile=gzip.open(args.vcffile[:-7]+".annotated.vcf.gz",'w')
     else:
         outputfile=open(args.vcffile[:-4]+".annotated.vcf",'w')
 
-    vcf_writer = vcf.Writer(outputfile, vcf_reader)
+    vcf_writer = pysam.VariantFile(outputfile, 'w', header=vcf_reader.header)
 
     try:
         for record in vcf_reader:
 
-            if record.INFO['diffsize']>=args.mindiff and record.INFO['diffsize']<args.maxdiff:
-                # key=str(record.CHROM+"_"+str(record.POS))
-                key=(record.CHROM,record.POS)
+            if record.info['diffsize']>=args.mindiff and record.info['diffsize']<args.maxdiff:
+                key=(record.chrom,record.pos)
 
                 if key in repmd: #we have a repeat masker annotation for this allele
-                    record.INFO['repm_rfamily']=repmd[key]['rfamily']
-                    record.INFO['repm_rtype']=repmd[key]['rtype']
-                    record.INFO['repm_rcov']=repmd[key]['rcov']
-                    record.INFO['repm_qcov']=repmd[key]['qcov']
-                    record.INFO['repm_allele']=repmd[key]['allele'] #numeric representation of the allele that this annotation was based on
+                    record.info['repm_rfamily']=repmd[key]['rfamily']
+                    record.info['repm_rtype']=repmd[key]['rtype']
+                    record.info['repm_rcov']=repmd[key]['rcov']
+                    record.info['repm_qcov']=repmd[key]['qcov']
+                    record.info['repm_allele']=repmd[key]['allele'] #numeric representation of the allele that this annotation was based on
 
                 if key in trfd: #we have a trf annotation for this allele
-                    record.INFO['trf_copynumber']=trfd[key]['copynumber']
-                    record.INFO['trf_conssize']=trfd[key]['cons_size']
-                    record.INFO['trf_entropy']=trfd[key]['entropy']
-                    record.INFO['trf_start']=trfd[key]['start']
-                    record.INFO['trf_end']=trfd[key]['end']
-                    record.INFO['trf_pattern']=trfd[key]['pattern']
-                    record.INFO['trf_percent_indel']=trfd[key]['percent_indel']
-                    record.INFO['trf_percent_match']=trfd[key]['percent_match']
-                    record.INFO['trf_gccontent']=(trfd[key]['G']+trfd[key]['C'])/float(trfd[key]['A']+trfd[key]['C']+trfd[key]['G']+trfd[key]['T'])
-                    record.INFO['trf_allele']=trfd[key]['allele'] #numeric representation of the allele that this annotation was based on
+                    record.info['trf_copynumber']=trfd[key]['copynumber']
+                    record.info['trf_conssize']=trfd[key]['cons_size']
+                    record.info['trf_entropy']=trfd[key]['entropy']
+                    record.info['trf_start']=trfd[key]['start']
+                    record.info['trf_end']=trfd[key]['end']
+                    record.info['trf_pattern']=trfd[key]['pattern']
+                    record.info['trf_percent_indel']=trfd[key]['percent_indel']
+                    record.info['trf_percent_match']=trfd[key]['percent_match']
+                    record.info['trf_gccontent']=(trfd[key]['G']+trfd[key]['C'])/float(trfd[key]['A']+trfd[key]['C']+trfd[key]['G']+trfd[key]['T'])
+                    record.info['trf_allele']=trfd[key]['allele'] #numeric representation of the allele that this annotation was based on
 
                 #add custom reveal annotation derived from repeatmasker and trf annotations
                 if key in repmd and repmd[key]['rcov']>0.8 and repmd[key]['qcov']>0.8 and repmd[key]['rfamily'].startswith('Satellite'): #repeat annotation and allele have reciprocal overlap >0.8
-                    record.INFO['reveal_type']='mei'
-                elif key in trfd and (len(trfd[key]['masked'])/record.INFO['diffsize'])>0.5: #no mei, but more than 50% of the indel size can be attributed to tandemly repeated sequence
+                    record.info['reveal_type']='mei'
+                elif key in trfd and (len(trfd[key]['masked'])/record.info['diffsize'])>0.5: #no mei, but more than 50% of the indel size can be attributed to tandemly repeated sequence
                     if trfd[key]['cons_size']==1:
-                        record.INFO['reveal_type']='homopolymer'
+                        record.info['reveal_type']='homopolymer'
                     elif trfd[key]['cons_size']<=6:
-                        record.INFO['reveal_type']='micro-satellite'
+                        record.info['reveal_type']='micro-satellite'
                     elif trfd[key]['cons_size']<100:
-                        record.INFO['reveal_type']='mini-satellite'
+                        record.info['reveal_type']='mini-satellite'
                     elif trfd[key]['cons_size']<1000:
-                        record.INFO['reveal_type']='macro-satellite'
+                        record.info['reveal_type']='macro-satellite'
                     elif trfd[key]['cons_size']>1000:
-                        record.INFO['reveal_type']='mega-satellite'
+                        record.info['reveal_type']='mega-satellite'
                 else:
-                    record.INFO['reveal_type']='other'
+                    record.info['reveal_type']='other'
 
-            vcf_writer.write_record(record)
+            vcf_writer.write(record)
 
     except IOError:
-        print "IOError"
         pass
 
 
@@ -159,7 +154,7 @@ def load_trf_annotations(trffile,aid2variant):
     trfd={}
     pvariant=None
     trfcolnames=['start','end','period_size','copynumber','cons_size','percent_match','percent_indel','score','A','C','G','T','entropy','pattern','masked']
-    trfcoltypes=[int,int,int,float,int,int,int,int,int,int,int,int,float,str,str]
+    trfcoltypes=[str,str,int,float,int,int,int,int,int,int,int,int,float,str,str]
     #13 47 1 35.0 1 100 0 70 0 0 0 100 0.00 T TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
 
     with open(trffile) as trfout:
